7bfc3c83385b0e7edc9db07e7ee184f4e57e38c2
ajaynagesh <ajaynagesh@gmail.com>
2015-08-02 08:35:32 +0530

    Adding files for setup.sh

---
+++ latentssvm_relext.mosek.dd/setup.sh
@@ -0,0 +1,3 @@
+export MOSEKLM_LICENSE_FILE=~/Research/software/mosek.5/mosek.lic
+export LD_LIBRARY_PATH='/home/ajay/Research/software/lp_solve/:/home/ajay/Research/software/mosek.5/5/tools/platform/linux64x86/bin/:/home/ajay/Research/software/x86-64_sles10_4.1/'
+
